2b1636170285712ac157df4c3ad3d798956ab2a6
Jay Miller <kjaymiller@gmail.com>
2022-03-02 06:56:13 -0800

    Jm call eui assets (#1)
    * add yarn and update readme
    
    * Object page Base Template
    
    * add eui dependencies to package.json
    
    Co-authored-by: Brittany Joiner <brittanyjoiner15@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # es-civics-data-visualizations
 
-Wooohooo!
+A project to show stop and frisk and other civics data in various populations in US. We'll be hosting data in Elasticsearch and building a SPA to visualize the data with the Eui Design Library (including EuiCharts)
+
+Currently being worked on by @kjaymiller and @brittanyjoiner15
+
+## Instructions for Running Locally
+
+- clone this repo
+- from the root, run `yarn install` or `npm install`
+- to start, run `yarn start` or `npm run dev`
--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { EuiProvider } from '@elastic/eui';
+import '@elastic/eui/dist/eui_theme_light.css';
+import Header from './eui-header.js';
+import Topic from './eui-object-template.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EuiProvider colorMode='light'>
+      <Header />
+      <Topic />
+    </EuiProvider>
   );
 }
 
